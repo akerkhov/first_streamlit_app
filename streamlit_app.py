cd8625254caa53e930f31c20e77ea7adeabb1e47
akerkhov <106763961+akerkhov@users.noreply.github.com>
2022-06-02 13:33:52 -0500

    Update streamlit_app.py
    added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header ('breakfast menus')
-streamlit.text ('Omega 3 & Blueberry Oatmeal')
-streamlit.text ('Kale, Spinich & Rocket Smoothoe')
-streamlit.text ('Hard-bolied Free-Range Egg')
+streamlit.text ('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text ('🥗 Kale, Spinich & Rocket Smoothoe')
+streamlit.text ('🥚🐔Hard-bolied Free-Range Egg')
+steamlit.text('🥑🥑Avacado Toast')  
